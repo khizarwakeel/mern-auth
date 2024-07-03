dd3652319fcc6432128691f1ea725bf7f093cce8
Khizar Wakeel <khizarwakeel@gmail.com>
2024-07-03 16:31:50 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000 || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
