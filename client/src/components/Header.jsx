37ef85d1e0962e81c30e8752b7b7fa9d7412113d
Khizar Wakeel <khizarwakeel@gmail.com>
2024-07-08 19:54:49 +0500

    add redux persist

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,14 +7,18 @@ const Header = () => {
       <div className="flex items-center justify-between max-w-7xl mx-auto px-5 md:py-3 py-5">
         <Link to={"/"} className="flex items-center gap-2">
           <div>
-            <img src="/assets/logo.png" alt="MERN Auth" className="md:w-12 w-6" />
+            <img
+              src="/assets/logo.png"
+              alt="MERN Auth"
+              className="md:w-12 w-6"
+            />
           </div>
           <div>
             <h4 className="md:text-3xl text-sm">MERN Auth</h4>
           </div>
         </Link>
         <div>
-          <ul className="flex md:gap-6 gap-2 text-gray-600">
+          <ul className="flex md:gap-10 gap-3 text-gray-60 text-sm md:text-base">
             <Link to={"/"} className="hover:text-[#2680f0]">
               <li>Home</li>
             </Link>
