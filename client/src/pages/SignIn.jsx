506c2fda3a1a88e3f123db6025553960bd1f76f7
Khizar Wakeel <khizarwakeel@gmail.com>
2024-07-08 20:40:37 +0500

    removed signin error message

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -137,7 +137,7 @@ export default function SignIn() {
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
               </svg>
               <span className="sr-only">Info</span>
-              <div>{error || error.message || "Something went wrong!"}</div>
+              <div>{error.message || "Something went wrong!"}</div>
             </div>
           )}
         </div>
