3f93ad518bda6916eced5d961c86ae3097f73f43
Khizar Wakeel <khizarwakeel@gmail.com>
2024-07-11 18:36:47 +0500

    create update user route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const port = 3000 || 8000;
+
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO_URI)
