d5a9a92d690f73eb4251ee9359a348aea9e7c61d
Khizar Wakeel <khizarwakeel@gmail.com>
2024-07-12 17:20:00 +0500

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,10 +4,18 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const app = express();
 const port = 3000 || 8000;
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use(cookieParser());
